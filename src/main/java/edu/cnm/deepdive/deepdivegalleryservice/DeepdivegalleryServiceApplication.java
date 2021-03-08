362df7a591cbd3f646daaa40f064235151daf7c5
rbrazell1 <brazellrussell@gmail.com>
2021-03-08 11:59:28 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/deepdivegalleryservice/DeepdivegalleryServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.deepdivegalleryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepdivegalleryServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeepdivegalleryServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/deepdivegalleryservice/DeepdivegalleryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.deepdivegalleryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepdivegalleryServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
