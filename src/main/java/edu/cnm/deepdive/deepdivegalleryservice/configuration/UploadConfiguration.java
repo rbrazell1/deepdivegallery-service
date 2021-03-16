fc9e59046a36109705ffc95fc695df8f10801d2a
rbrazell1 <brazellrussell@gmail.com>
2021-03-16 10:47:18 -0600

    created class to set Upload Configuration

---
+++ src/main/java/edu/cnm/deepdive/deepdivegalleryservice/configuration/UploadConfiguration.java
@@ -0,0 +1,126 @@
+package edu.cnm.deepdive.deepdivegalleryservice.configuration;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TimeZone;
+import java.util.regex.Pattern;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "upload")
+public class UploadConfiguration {
+
+  private boolean applicationHome = true;
+  private String directory = "uploads";
+  private Pattern subdirectoryPattern = Pattern.compile("^(.{4})(.{2})(.{2}).*$");
+  private Set<String> whitelist = new LinkedHashSet<>(
+      Set.of(
+          "image/bmp",
+          "image/gif",
+          "image/jpeg",
+          "image/pjpeg",
+          "image/png",
+          "image/tiff"
+      )
+  );
+  private FilenameProperties filename;
+
+  public boolean isApplicationHome() {
+    return applicationHome;
+  }
+
+  public void setApplicationHome(boolean applicationHome) {
+    this.applicationHome = applicationHome;
+  }
+
+  public String getDirectory() {
+    return directory;
+  }
+
+  public void setDirectory(String directory) {
+    this.directory = directory;
+  }
+
+  public Pattern getSubdirectoryPattern() {
+    return subdirectoryPattern;
+  }
+
+  public void setSubdirectoryPattern(Pattern subdirectoryPattern) {
+    this.subdirectoryPattern = subdirectoryPattern;
+  }
+
+  public Set<String> getWhitelist() {
+    return whitelist;
+  }
+
+  public void setWhitelist(Set<String> whitelist) {
+    this.whitelist = whitelist;
+  }
+
+  public FilenameProperties getFilename() {
+    return filename;
+  }
+
+  public void setFilename(FilenameProperties filename) {
+    this.filename = filename;
+  }
+
+  public static class FilenameProperties {
+
+    private String format = "%1$s-%2$d.%3$s";
+    private int randomizerLimit = 1_000_000;
+    private TimestampProperties timestampProperties;
+
+
+    public String getFormat() {
+      return format;
+    }
+
+    public void setFormat(String format) {
+      this.format = format;
+    }
+
+    public int getRandomizerLimit() {
+      return randomizerLimit;
+    }
+
+    public void setRandomizerLimit(int randomizerLimit) {
+      this.randomizerLimit = randomizerLimit;
+    }
+
+    public TimestampProperties getTimestampProperties() {
+      return timestampProperties;
+    }
+
+    public void setTimestampProperties(TimestampProperties timestampProperties) {
+      this.timestampProperties = timestampProperties;
+    }
+  }
+
+  public static class TimestampProperties {
+
+    private String format = "yyyyMMddHHmmssSSS";
+    private TimeZone timeZone = TimeZone.getTimeZone("UTC");
+
+
+    public String getFormat() {
+      return format;
+    }
+
+    public void setFormat(String format) {
+      this.format = format;
+    }
+
+    public TimeZone getTimeZone() {
+      return timeZone;
+    }
+
+    public void setTimeZone(TimeZone timeZone) {
+      this.timeZone = timeZone;
+    }
+  }
+
+}
+
+
